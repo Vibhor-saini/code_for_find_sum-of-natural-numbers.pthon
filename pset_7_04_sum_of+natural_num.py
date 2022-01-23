070fca22230c58260dcd08f0fe9a515d4a226994
Vibhor saini <98160883+Vibhor-saini@users.noreply.github.com>
2022-01-22 22:31:06 -0800

    Add files via upload

---
+++ pset_7_04_sum_of+natural_num.py
@@ -0,0 +1,15 @@
+n = int(input("Enter the number : "))
+i = 1
+sum = 0
+while i<=n:
+    sum = sum + i
+    i = i + 1
+print(f"The sum of first {n} numbers is = {sum}")
+
+                        # using for lop
+
+# n = int(input("Enter the number : "))
+# sum = 0
+# for i in range(1,n+1):
+#  sum = sum + i
+# print("sum is",sum)
